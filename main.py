40e1ea3034eeaef8a4364f9e5de6ca11d6ec76ff
Amilcar Salvatierra <raclima977@gmail.com>
2025-12-28 18:20:22 -0500

    Add POST endpoint to receive IoT data

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
-def mensaje_de_bienvenida():
-    return {"status": "Servidor de Electronica Activo", "docente": "Peru"}
+# Definimos qué datos esperamos del ESP32
+class DatosIoT(BaseModel):
+    sensor: str
+    valor: float
 
-@app.get("/lectura/{valor}")
-def recibir_sensor(valor: float):
-    return {"mensaje": "Dato recibido", "valor_del_esp32": valor}
+@app.get("/")
+def inicio():
+    return {"mensaje": "Servidor activo. Listo para recibir datos del ESP32"}
+
+# Esta es la "puerta" que falta para corregir el error 405
+@app.post("/")
+async def recibir_datos(datos: DatosIoT):
+    print(f"Dato recibido de {datos.sensor}: {datos.valor}")
+    return {"estado": "recibido", "valor": datos.valor}
